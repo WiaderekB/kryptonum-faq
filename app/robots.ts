3cd39855af1e0d735dcd10c8a67e8708f501445a
Bartosz Wiaderek <64980866+WiaderekB@users.noreply.github.com>
2024-11-20 21:13:58 +0100

    Handle empty .env

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-	const baseUrl = process.env.PUBLIC_URL;
+	const baseUrl = process.env.PUBLIC_URL || "kryptonum-faq.vercel.app";
 	return {
 		rules: {
 			userAgent: "*",
