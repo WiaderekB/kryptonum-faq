a4105cf5a81be2b0132107bfb56fafc0bdeb640f
Bartosz Wiaderek <64980866+WiaderekB@users.noreply.github.com>
2024-11-20 20:20:25 +0100

    Add aria tags

--- components/Faq/Question/question.tsx
+++ components/Faq/Question/question.tsx
@@ -5,24 +5,29 @@ import { FaqQuestionDataType } from "./question.types";
 
 function FaqQuestion(props: { index: number; questionData: FaqQuestionDataType }) {
 	const [open, setOpen] = useState(false);
+	const questionId = `faq-question-${props.index}`;
+	const answerId = `faq-answer-${props.index}`;
+
 	return (
-		<div className={`${styles.question} ${open ? styles.open : ""}`}>
-			<p className={styles.index}>{props.index + 1 < 10 ? `0${props.index + 1}` : props.index + 1}</p>
+		<article className={`${styles.question} ${open ? styles.open : ""}`}>
+			<p className={styles.index} aria-hidden="true">
+				{props.index + 1 < 10 ? `0${props.index + 1}` : props.index + 1}
+			</p>
 
 			<div className={styles.content}>
 				<div className={styles.title}>
-					<h2>{props.questionData.question}</h2>
-					<button aria-label={open ? "Close answer" : "Open answer"} onClick={() => setOpen((previous) => !previous)}>
-						<span className={styles.horizontal}></span>
-						<span className={styles.vertical}></span>
+					<h2 id={questionId}>{props.questionData.question}</h2>
+					<button aria-expanded={open} aria-controls={answerId} onClick={() => setOpen((previous) => !previous)} aria-label={open ? "Close answer" : "Open answer"}>
+						<span className={styles.horizontal} aria-hidden="true"></span>
+						<span className={styles.vertical} aria-hidden="true"></span>
 					</button>
 				</div>
 
-				<div className={styles.answerWrapper}>
+				<div className={styles.answerWrapper} id={answerId} role="region" aria-labelledby={questionId}>
 					<p>{props.questionData.answer}</p>
 				</div>
 			</div>
-		</div>
+		</article>
 	);
 }
 
