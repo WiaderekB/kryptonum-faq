65f907380d42442ddb124f3f574ace1f85062204
Bartosz Wiaderek <64980866+WiaderekB@users.noreply.github.com>
2024-11-20 20:06:09 +0100

    Fix scss warnings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	sassOptions: {
+		additionalData: `@use "@/global/variables" as *;`,
+		silenceDeprecations: ["legacy-js-api"],
+	},
 };
 
 export default nextConfig;
